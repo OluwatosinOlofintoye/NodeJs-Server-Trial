dc0de5e61cecb49fccf19459fc1d5a86445a27c0
OluwatosinOlofintoye <84126747+OluwatosinOlofintoye@users.noreply.github.com>
2024-08-02 16:08:53 +0100

    added server files

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require("http")
+const express=require("express");
+const app = express()
+
+app.use(express.json({limit:50}))
+app.use(express.urlencoded({extended:true,limit:50}))
+function getAllUser(req,res){
+res.status(200).json({
+	user:{name:"first"}
+})	
+	
+}
+app.get("/api/user/all",getAllUser);
+const PORT=3000
+app.listen(PORT,()=>{
+	console.log("Server listening on port ",PORT)
+})
